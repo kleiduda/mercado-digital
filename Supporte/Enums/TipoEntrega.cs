b207818e4f23189f46c5d72db98ee05bb8c0b070
kleiduda <kleiduda@gmail.com>
2020-04-25 21:22:48 -0300

    comportamento do botao fechar caixa

---
+++ Supporte/Enums/TipoEntrega.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supporte.Enums
+{
+    public enum TipoEntrega : int
+    {
+        Balcão = 1,
+        Delivery = 2
+    }
+}
